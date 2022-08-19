46459014ccc14fd74a401f6198c90d994c714b79
scott-w-maxwell <scottwillardmaxwell@protonmail.com>
2022-08-19 09:10:25 -0700

    Created User Schema

---
+++ models/User.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+// Create Schema for Users
+const userSchema = new Schema({
+    email:{
+        type: String,
+        required: true
+    },
+    password:{
+        type: String,
+        required: true
+    }
+}, {timestamps: true})
+
+// Create model from Schema
+const User = mongoose.model('User', userSchema)
