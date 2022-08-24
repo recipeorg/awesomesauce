772a4cd52a29ff5c71dddbe36720d41cb9a80682
scott-w-maxwell <scottwillardmaxwell@protonmail.com>
2022-08-24 15:24:11 -0700

    Setup User Sign Up

--- app.js
+++ app.js
@@ -2,12 +2,6 @@
 const express = require('express')
 const app = express()
 
-// For connection to MongoDB
-const mongoose = require('mongoose')
-
-// For User Scheme
-const User = require('./models/user')
-
 // For environment variables
 require('dotenv/config')
 
@@ -19,25 +13,9 @@ app.get('/', (req, res) =>{
     res.render('index')
 })
 
+// Connects auth.js routes
 const authenticationRouter = require('./routes/auth')
-app.use('/auth', authenticationRouter)
-
-// connect to MongoDB
-mongoose.connect(
-    process.env.DB_CONNECTION, 
-    (err)=> {
-        if(err) console.log(err)
-        else console.log('Connected to MongoDB!')
-    }
-)
-
-// Code to create a user object and save to database:
-// const user = new User({
-//     'username': 'exampleuser',
-//     'email': 'example@gmail.com',
-//     'password': 'password123'
-// })
-// user.save()
+app.use('/', authenticationRouter)
 
 // Make App listen on port 3000
 app.listen(3000)
--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,57 @@
+const { application } = require('express')
 const express = require('express')
+const bcrypt = require('bcrypt')
 const router = express.Router()
+
+// For User Scheme
+const User = require('../models/user')
+
+router.use(express.urlencoded({ extended: false }))
+
+// For connection to MongoDB
+const mongoose = require('mongoose')
+
+// connect to MongoDB
+mongoose.connect(
+    process.env.DB_CONNECTION, 
+    (err)=> {
+        if(err) console.log(err)
+        else console.log('Connected to MongoDB!')
+    }
+)
 
 router.get('/login', (req, res) =>{
     res.render('login')
 })
 
+router.post('/login', (req, res) =>{
+    // req.body.email
+    // req.body.username
+    // req.body.password
+})
+
 router.get('/signup', (req, res) =>{
     res.render('signup')
+})
+
+router.post('/signup', async (req, res) =>{
+    try{
+        
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+
+        // Code to create a user object and save to database:
+        const user = new User({
+            'username': req.body.username,
+            'email': req.body.email,
+            'password': hashedPassword
+        })
+        user.save()
+        console.log('User Registered')   
+        
+        res.redirect('/login')
+    } catch{
+        res.redirect('/signup')
+    }
 })
 
 module.exports = router
